da6ecc6fa8b092bda933b4b6420028f1fff4a164
xingkongbj <xingkongbeijing@sina.com>
2017-08-17 11:27:56 +0800

    JS

--- 前端/JS/常用算法.js
+++ 前端/JS/常用算法.js
@@ -335,3 +335,25 @@ function vueMerge(t, obj1, obj2) {
     return obj1;
 }
 
+// 判断浏览器是否支持某个 css 属性
+function supportCss3(style) {
+    var prefix = ['webkit', 'Moz', 'ms', 'o'],
+        i,
+        humpString = [],
+        htmlStyle = document.documentElement.style,
+        _toHumb = function (string) {
+            return string.replace(/-(\w)/g, function ($0, $1) {
+                return $1.toUpperCase();
+            });
+        };
+
+    for (i in prefix)
+        humpString.push(_toHumb(prefix[i] + '-' + style));
+
+    humpString.push(_toHumb(style));
+
+    for (i in humpString)
+        if (humpString[i] in htmlStyle) return true;
+
+    return false;
+}
